f94a6e0515c2082c789a8d56508a8756d3627cfa
Anna W <anski.anna@gmail.com>
2019-07-24 14:25:25 +0200

    register endpoint working. Can register new user with hashed password

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const usersRouter = require('../users/users-router');
+const authRouter = require('../auth/auth-router');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send('welcome to auth project!')
---
+++ auth/auth-router.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'could not register new user'})
+        })
+})
+
+module.exports = router;
